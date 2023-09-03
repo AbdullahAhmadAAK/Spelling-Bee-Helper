68d9b182760803bd3c23d4fcb43f5823d7adbf97
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2023-09-04 03:55:11 +0500

    Improve coloring + UI, add navbar

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const addWordsList = () => {
 
     // add words to program's words_list
     words_array.forEach(word => {
-        if(!words_list.includes(word.toLowerCase())) {
+        if(words_list!=[] && !words_list.includes(word.toLowerCase())) {
             words_list.push(word.toLowerCase());
         }
     });
@@ -81,7 +81,6 @@ const addWordsList = () => {
     clearInputs();
 }
 const clearInputs = () => {
-    document.getElementById("new_word").value = "";
     document.getElementById("new_words_list").value = "";
 }
 const saveList = () => {
