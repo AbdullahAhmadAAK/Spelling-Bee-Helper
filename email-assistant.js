5cfe99710d6763dc0b866cac1eb395e407162489
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2023-09-23 14:52:01 +0500

    lay down basic form of service. 1 email now being sent

--- email-assistant.js
+++ email-assistant.js
@@ -6,14 +6,16 @@ let content = ""
 
 const saveSubjectContent = () => {
     //
-    subj = document.getElementById("subject")
-    content = document.getElementById("content")
+    subj = document.getElementById("subject").value
+    content = document.getElementById("content").value
 
     // render new form
     document.getElementById('left-col').innerHTML = `
     <form action="javascript:void(0)" onsubmit="saveEmail()">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
+        <label for="name">Company Name:</label>
+        <input type="text" id="name" name="name" required>
         <br><br>
         <button type="submit" class="btn btn-primary">Add Email</button>
         <button type="button" class="btn btn-success" onclick="startSending()">Start Sending</button>
@@ -32,8 +34,13 @@ const saveEmail = () => {
 }
 
 const startSending = () => {
-    // // prep email
-    // const contentInstance = content.replace(/COMPANY/g, )
-    // // display it
-    // document.getElementById('left-col').innerHTML = `<h1>${subj}</h1><p>${content}</p>`
+    // prep email
+    const company_obj = companies_list.pop();
+    console.log(company_obj)
+
+    const contentInstance = content.replace(/COMPANYNAME/g, company_obj.name)
+    const emailInstance = company_obj.email
+
+    // display it
+    document.getElementById('left-col').innerHTML = `<h3>${subj}</h3><h4>To ${emailInstance}</h4><p>${contentInstance}</p>`
 }
