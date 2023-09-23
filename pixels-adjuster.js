f988ec095a6f499fcaf79b04d6ea745722fc0d8f
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2023-09-23 16:52:57 +0500

    add model input fields for pixels adjuster

---
+++ pixels-adjuster.js
@@ -0,0 +1,8 @@
+let model_width = 0;
+let model_height = 0;
+
+const savePixels = () => {
+    model_width = document.getElementById("model_width");
+    model_height = document.getElementById("model_height")
+    console.log('i got called')
+}
